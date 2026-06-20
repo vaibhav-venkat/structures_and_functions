4cc03e0d8a44613c64142a323b8c71728b468635
Vaibhav Venkat <vaibhav@vaibhavvenkat.com>
2026-06-20 12:51:34 -0700

    Change amount of info put out

--- hexatic/cylinder_dynamics/script.py
+++ hexatic/cylinder_dynamics/script.py
@@ -45,7 +45,7 @@ def main() -> None:
     animate_outer_shell_xtheta_x_velocity(
         CYLINDER_PATHS.in_gsd,
         CYLINDER_PATHS.shell_xtheta_x_velocity_movie,
-        start_frame=100,
+        start_frame=0,
     )
     print(
         "Wrote outer-shell x-theta x velocity movie to "
@@ -54,64 +54,64 @@ def main() -> None:
     animate_outer_shell_xtheta_theta_velocity(
         CYLINDER_PATHS.in_gsd,
         CYLINDER_PATHS.shell_xtheta_theta_velocity_movie,
-        start_frame=100,
+        start_frame=0,
     )
     print(
         "Wrote outer-shell x-theta theta velocity movie to "
         f"{CYLINDER_PATHS.shell_xtheta_theta_velocity_movie}."
     )
-    # plot_disclination_center_of_mass_series(
-    #     CYLINDER_PATHS.in_gsd,
-    #     CYLINDER_PATHS.neighbor_count_txt,
-    #     CYLINDER_PATHS.disclination_com_plot,
-    # )
-    # print(
-    #     "Wrote disclination center-of-mass plot to "
-    #     f"{CYLINDER_PATHS.disclination_com_plot}."
-    # )
-    # plot_dislocation_center_of_mass_series(
-    #     CYLINDER_PATHS.in_gsd,
-    #     CYLINDER_PATHS.neighbor_count_txt,
-    #     CYLINDER_PATHS.dislocation_com_plot,
-    # )
-    # print(
-    #     "Wrote dislocation center-of-mass plot to "
-    #     f"{CYLINDER_PATHS.dislocation_com_plot}."
-    # )
-    # plot_dislocation_count_series(
-    #     CYLINDER_PATHS.in_gsd,
-    #     CYLINDER_PATHS.neighbor_count_txt,
-    #     CYLINDER_PATHS.dislocation_count_plot,
-    # )
-    # print(f"Wrote dislocation count plot to {CYLINDER_PATHS.dislocation_count_plot}.")
-    # plot_disclination_count_series(
-    #     CYLINDER_PATHS.in_gsd,
-    #     CYLINDER_PATHS.neighbor_count_txt,
-    #     CYLINDER_PATHS.disclination_count_plot,
-    # )
-    # print(f"Wrote disclination count plot to {CYLINDER_PATHS.disclination_count_plot}.")
-    # plot_net_disclination_charge_series(
-    #     CYLINDER_PATHS.in_gsd,
-    #     CYLINDER_PATHS.neighbor_count_txt,
-    #     CYLINDER_PATHS.net_charge_plot,
-    # )
-    # print(f"Wrote net disclination charge plot to {CYLINDER_PATHS.net_charge_plot}.")
+    plot_disclination_center_of_mass_series(
+        CYLINDER_PATHS.in_gsd,
+        CYLINDER_PATHS.neighbor_count_txt,
+        CYLINDER_PATHS.disclination_com_plot,
+    )
+    print(
+        "Wrote disclination center-of-mass plot to "
+        f"{CYLINDER_PATHS.disclination_com_plot}."
+    )
+    plot_dislocation_center_of_mass_series(
+        CYLINDER_PATHS.in_gsd,
+        CYLINDER_PATHS.neighbor_count_txt,
+        CYLINDER_PATHS.dislocation_com_plot,
+    )
+    print(
+        "Wrote dislocation center-of-mass plot to "
+        f"{CYLINDER_PATHS.dislocation_com_plot}."
+    )
+    plot_dislocation_count_series(
+        CYLINDER_PATHS.in_gsd,
+        CYLINDER_PATHS.neighbor_count_txt,
+        CYLINDER_PATHS.dislocation_count_plot,
+    )
+    print(f"Wrote dislocation count plot to {CYLINDER_PATHS.dislocation_count_plot}.")
+    plot_disclination_count_series(
+        CYLINDER_PATHS.in_gsd,
+        CYLINDER_PATHS.neighbor_count_txt,
+        CYLINDER_PATHS.disclination_count_plot,
+    )
+    print(f"Wrote disclination count plot to {CYLINDER_PATHS.disclination_count_plot}.")
+    plot_net_disclination_charge_series(
+        CYLINDER_PATHS.in_gsd,
+        CYLINDER_PATHS.neighbor_count_txt,
+        CYLINDER_PATHS.net_charge_plot,
+    )
+    print(f"Wrote net disclination charge plot to {CYLINDER_PATHS.net_charge_plot}.")
     write_active_matter_field_outputs(
         CYLINDER_PATHS.in_gsd,
         coordinate_system="xrtheta"
     )
     print(
         "Wrote active matter flux density plots to "
-        f"{ACTIVE_IMAGE_DIR / 'flux' / 'cartesian'}."
+        f"{ACTIVE_IMAGE_DIR / 'flux'}."
     )
-    # write_chirality_outputs(
-    #     CYLINDER_PATHS.in_gsd,
-    #     config = ChiralityConfig(limit_disclination=True)
-    # )
-    # print(
-    #     "Wrote disclination chirality fields to "
-    #     f"{CHIRALITY_DATA_DIR / 'chirality_disclinations'}."
-    # )
+    write_chirality_outputs(
+        CYLINDER_PATHS.in_gsd,
+        config = ChiralityConfig(limit_disclination=True)
+    )
+    print(
+        "Wrote disclination chirality fields to "
+        f"{CHIRALITY_DATA_DIR / 'chirality_disclinations'}."
+    )
     print("OVITO position.x stores x")
     print("OVITO position.y stores theta")
     print("OVITO position.z stores r")
